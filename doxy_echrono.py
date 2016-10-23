ffd3e310cc41555091eb44abf57d48c68865ff96
Edouard DUPIN <yui.heero@gmail.com>
2016-10-23 14:55:24 +0200

    [RELEASE] new version 0.1.0

--- doxy_echrono.py
+++ doxy_echrono.py
@@ -7,14 +7,14 @@ import doxy.tools as tools
 def create(target, module_name):
 	my_module = module.Module(__file__, module_name)
 	my_module.set_version("version.txt")
-	my_module.set_title("E-translate: Ewol runtime translate tool")
+	my_module.set_title("E-chrono: Ewol chrono wrapper")
 	my_module.set_website("http://atria-soft.github.io/" + module_name)
 	my_module.set_website_sources("http://github.com/atria-soft/" + module_name)
-	"""
 	my_module.add_path([
 	    module_name,
 	    "doc"
 	    ])
+	"""
 	my_module.add_sample_path([
 	    "sample"
 	    ])
@@ -22,7 +22,6 @@ def create(target, module_name):
 	my_module.add_depend([
 	    'elog',
 	    'etk',
-	    'ejson',
 	    ])
 	my_module.add_exclude_symbols([
 	    '*operator<<*',
