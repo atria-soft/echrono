1ac238922c5b1a25ae58bfd31d19dd506ead4757
Edouard DUPIN <yui.heero@gmail.com>
2017-01-05 21:28:23 +0100

    Change licence APACHE-2 to MPL-2 ==> force source redistribution and permit static link

--- lutin_echrono.py
+++ lutin_echrono.py
@@ -10,7 +10,7 @@ def get_desc():
 	return "E-chrono: Ewol chrono wrapper to simplify use of std::chrono (and permit to create a virtual clock)"
 
 def get_licence():
-	return "APACHE-2"
+	return "MPL-2"
 
 def get_compagny_type():
 	return "com"
